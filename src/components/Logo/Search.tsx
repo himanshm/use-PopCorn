e86c8d0a6c38d959ca07c3d1adaffd29039a22e3
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-25 12:08:25 +0530

    Use ref to select dom elements

--- src/components/Logo/Search.tsx
+++ src/components/Logo/Search.tsx
@@ -1,10 +1,29 @@
-import { type ChangeEvent } from 'react';
+import { useEffect, useRef, type ChangeEvent } from 'react';
 
 type SearchProps = {
   query: string;
   handleSetQuery: (value: string) => void;
 };
 function Search({ query, handleSetQuery }: SearchProps) {
+  const inputElement = useRef<HTMLInputElement>(null);
+
+  /* we need to use an effect in order to use a ref that contains a DOM element like this one because the ref only gets added to this DOM element here after the DOM has already loaded, therefore we can only access it in effect which also runs after the DOM has been loaded. So this is the perfect place for using a ref that contains a DOM element. */
+
+  useEffect(() => {
+    function callback(event: KeyboardEvent) {
+      if (document.activeElement === inputElement.current) return;
+      if (event.code === 'Enter') {
+        inputElement.current!.focus();
+        handleSetQuery('');
+      }
+    }
+
+    document.addEventListener('keydown', callback);
+
+    return () => {
+      document.addEventListener('keydown', callback);
+    };
+  }, [handleSetQuery]);
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     handleSetQuery(event.target.value);
   }
@@ -15,6 +34,7 @@ function Search({ query, handleSetQuery }: SearchProps) {
       placeholder='Search movies...'
       value={query}
       onChange={handleChange}
+      ref={inputElement}
     />
   );
 }
